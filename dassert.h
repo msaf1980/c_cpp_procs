e91dd49599963d93f0e5f1df19541a89c6a33f0d
Michail Safronov <ms1980@inbox.ru>
2018-06-05 00:18:55 +0500

    Add files via upload

--- dassert.h
+++ dassert.h
@@ -3,7 +3,7 @@
  * If use DWARN, exit with WARNCOUNT from main()
  
 ..
-#define DASSERT_VERBOSE 1
+#define DWARN_VERBOSE 1
 #include <dassert.h>
 
 static size_t WARNCOUNT = 0;
@@ -18,8 +18,8 @@ int main()
 }
  */
 
-#ifndef DASSERT_VERBOSE
-#define DASSERT_VERBOSE 0
+#ifndef DWARN_VERBOSE
+#define DWARN_VERBOSE 0
 #endif
  
 #define DASSERT(x, msg) if (!(x)) { fprintf(stderr, "FAIL %s:%d: (%s) %s : %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg); abort(); }
@@ -28,15 +28,15 @@ int main()
 
 #define DWARN(x, msg) if (!(x)) { fprintf(stderr, "FAIL %s:%d: (%s) %s : %s\n", \
                                         __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg); WARNCOUNT++; } \
-                      else if (DASSERT_VERBOSE) { fprintf(stderr, "PASS %s:%d: (%s) %s : %s\n", \
+                      else if (DWARN_VERBOSE) { fprintf(stderr, "PASS %s:%d: (%s) %s : %s\n", \
                                         __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg); }
 
 #define DWARN_U(x, msg, count) if (!(x)) { fprintf(stderr, "FAIL %s:%d: (%s) %s : %s (%lu)\n", \
                                         __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg, count); WARNCOUNT++; } \
-                      else if (DASSERT_VERBOSE) { fprintf(stderr, "PASS %s:%d: (%s) %s : %s\n", \
+                      else if (DWARN_VERBOSE) { fprintf(stderr, "PASS %s:%d: (%s) %s : %s\n", \
                                         __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg); }
 
 #define DWARN_S(x, msg, count) if (!(x)) { fprintf(stderr, "FAIL %s:%d: (%s) %s : %s (%ld)\n", \
                                         __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg, count); WARNCOUNT++; } \
-                      else if (DASSERT_VERBOSE) { fprintf(stderr, "PASS %s:%d: (%s) %s : %s\n", \
+                      else if (DWARN_VERBOSE) { fprintf(stderr, "PASS %s:%d: (%s) %s : %s\n", \
                                         __FILE__, __LINE__, __PRETTY_FUNCTION__, #x, msg); }
