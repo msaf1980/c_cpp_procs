c997fd24c21e3d14356ee3e49dc149a123b56c72
Michail Safronov <msaf1980@gmail.com>
2019-04-12 00:45:55 +0500

    daemon_init redefinition

--- include/daemonutils.h
+++ include/daemonutils.h
@@ -4,15 +4,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
+
+#define FD_NOCLOSE 0
+#define FD_CLOSE_STD 1
+#define FD_CLOSE_ALL 2
+
 /* !
 Fork and init daemon child process
-\param[in] foreground 0 - ze pprocess, 1 - run foreground
-\param[in] nochdir 0 - chdir to / dir, 1 - don't chdir
-\param[in] noclose 0 - close STDIN, STDOUT, STDERR, 1 - don't close
+\param[in] background 1 - fork and detach process, 0 - run foreground
+\param[in] changedir 1 - chdir to / dir, 0 - don't chdir
+\param[in] closefd FD_NOCLOSE - don't close file descriptors, FD_CLOSE_STD - close STDIN, STDOUT, STDERR, FD_CLOSE_ALL - close all file descriptors
 \return 0 - success in forked process, > 0 - success fork (returned child pid), -1 - failed
  */
-int daemon_init(const int foreground, const int nochdir, const int noclose);
+int daemon_init(const int background, const int changedir, const int closefd);
 
 #ifdef __cplusplus
 }
--- src/daemonutils.c
+++ src/daemonutils.c
@@ -1,22 +1,32 @@
 #include <fcntl.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <unistd.h>
 
-int daemon_init(const int foreground, const int nochdir, const int noclose) {
+#include <daemonutils.h>
+
+int daemon_init(const int background, const int changedir, const int closefd) {
   int fd0, fd1, fd2;
   int pid, i;
 
-  if (foreground == 0) {
+  if (background) {
     if ((pid = fork()) == -1) { /* fork failed */
       perror("fork");
       return (pid);
     } else if (pid > 0) { /* fork success */
       return (pid);
     }
+    /* Get a new process group */
+    if (setsid() == -1) {
+      perror("can't setsid");
+      return -1;
+    }
   }
 
-  if (!nochdir) {
+  if (changedir) {
     if (chdir("/") == -1) {
       perror("can't chdir to root dir");
       return -1;
@@ -25,15 +35,25 @@ int daemon_init(const int foreground, const int nochdir, const int noclose) {
 
   umask(027); /* Set file permissions 750 */
 
-  /* Get a new process group */
-  if (setsid() == -1) {
-    perror("can't setsid");
-    return -1;
-  }
-
   /* Close  STDIN, STDOUT and STDERROR descriptors */
-  if (!noclose) {
-    for (i = 2; i >= 0; i--)
+  if (closefd != FD_NOCLOSE) {
+    size_t maxclosefd;
+    if (closefd == FD_CLOSE_STD)
+      maxclosefd = 2;
+    else {
+      /* Get maximum number of file descriptors. */ 
+      struct rlimit rl;
+      if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+        perror("can't get file limit");
+        return -1;
+      }
+      if (rl.rlim_max == RLIM_INFINITY)
+        maxclosefd = 1024;
+      else
+        maxclosefd = rl.rlim_max;
+    }
+    
+    for (i = maxclosefd; i >= 0; i--)
       close(i);
 
     /* Set file descriptors 0, 1 и 2 to /dev/null */
