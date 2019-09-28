0827ab129a5fc38b0f10fb31b31ac6049d888908
Michail Safronov <msaf1980@gmail.com>
2019-09-28 12:39:40 +0500

    add ignore_sigpipe

--- include/c_procs/daemonutils.h
+++ include/c_procs/daemonutils.h
@@ -9,12 +9,25 @@ extern "C" {
 #define FD_CLOSE_STD 1
 #define FD_CLOSE_ALL 2
 
-/* !
+/*
+ * Set the process's signal mask to ignore SIGPIPE signals. Returns 0
+ * if success, -1 if error, in which case the errno code is left in
+ * errno.
+ */
+int ignore_sigpipe();
+
+/*
 Fork and init daemon child process
-\param[in] background 1 - fork and detach process, 0 - run foreground
-\param[in] changedir 1 - chdir to / dir, 0 - don't chdir
-\param[in] closefd FD_NOCLOSE - don't close file descriptors, FD_CLOSE_STD - close STDIN, STDOUT, STDERR, FD_CLOSE_ALL - close all file descriptors
-\return 0 - success in forked process, > 0 - success fork (returned child pid), -1 - failed
+	param[in] background 1 - fork and detach process, 0 - run foreground
+	param[in] changedir 1 - chdir to / dir, 0 - don't chdir
+	param[in] closefd 
+		FD_NOCLOSE - don't close file descriptors
+		FD_CLOSE_STD - close STDIN, STDOUT, STDERR
+		FD_CLOSE_ALL - close all file descriptors
+return
+	0 - success in forked process or in foreground mode
+	> 0 - success fork (returned child pid)
+	-1 - failed
  */
 int daemon_init(const int background, const int changedir, const int closefd);
 
--- src/daemonutils.c
+++ src/daemonutils.c
@@ -4,64 +4,73 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <signal.h>
 #include <unistd.h>
 
 #include <c_procs/daemonutils.h>
 
+int ignore_sigpipe() {
+	struct sigaction sa, osa;
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	return sigaction(SIGPIPE, &sa, &osa);
+}
+
 int daemon_init(const int background, const int changedir, const int closefd) {
-    int fd0, fd1, fd2;
-    int pid, i;
+	int fd0, fd1, fd2;
+	int pid, i;
 
-    if (background) {
-        if ((pid = fork()) == -1) { /* fork failed */
-            perror("fork");
-            return (pid);
-        } else if (pid > 0) { /* fork success */
-            return (pid);
-        }
-        /* Get a new process group */
-        if (setsid() == -1) {
-            perror("can't setsid");
-            return -1;
-        }
-    }
+	if (background) {
+		if ((pid = fork()) == -1) { /* fork failed */
+			perror("fork");
+			return (pid);
+		} else if (pid > 0) { /* fork success */
+			return (pid);
+		}
+		/* Get a new process group */
+		if (setsid() == -1) {
+			perror("can't setsid");
+			return -1;
+		}
+	}
 
-    if (changedir) {
-        if (chdir("/") == -1) {
-            perror("can't chdir to root dir");
-            return -1;
-        }
-    }
+	if (changedir) {
+		if (chdir("/") == -1) {
+			perror("can't chdir to root dir");
+			return -1;
+		}
+	}
 
-    umask(027); /* Set file permissions 750 */
+	umask(027); /* Set file permissions 750 */
 
-    /* Close  STDIN, STDOUT and STDERROR descriptors */
-    if (closefd != FD_NOCLOSE) {
-        size_t maxclosefd;
-        if (closefd == FD_CLOSE_STD)
-            maxclosefd = 2;
-        else {
-            /* Get maximum number of file descriptors. */
-            struct rlimit rl;
-            if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
-                perror("can't get file limit");
-                return -1;
-            }
-            if (rl.rlim_max == RLIM_INFINITY)
-                maxclosefd = 1024;
-            else
-                maxclosefd = rl.rlim_max;
-        }
+	/* Close  STDIN, STDOUT and STDERROR descriptors */
+	if (closefd != FD_NOCLOSE) {
+		size_t maxclosefd;
+		if (closefd == FD_CLOSE_STD)
+			maxclosefd = 2;
+		else {
+			/* Get maximum number of file descriptors. */
+			struct rlimit rl;
+			if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+				perror("can't get file limit");
+				return -1;
+			}
+			if (rl.rlim_max == RLIM_INFINITY)
+				maxclosefd = 1024;
+			else
+				maxclosefd = rl.rlim_max;
+		}
 
-        for (i = maxclosefd; i >= 0; i--)
-            close(i);
+		for (i = maxclosefd; i >= 0; i--)
+			close(i);
 
-        /* Set file descriptors 0, 1 и 2 to /dev/null */
-        /* 'fd0' should be 0 */
-        fd0 = open("/dev/null", O_RDWR);
-        fd1 = dup(STDIN_FILENO);
-        fd2 = dup(STDIN_FILENO);
-    }
+		/* Set file descriptors 0, 1 и 2 to /dev/null */
+		/* 'fd0' should be 0 */
+		fd0 = open("/dev/null", O_RDWR);
+		fd1 = dup(STDIN_FILENO);
+		fd2 = dup(STDIN_FILENO);
+	}
 
-    return 0;
+	return 0;
 }
