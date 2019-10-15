a86713b1a16a9fd424834f3bfb9b998473137d7c
Michail Safronov <msaf1980@gmail.com>
2019-10-15 22:38:27 +0500

    strerror2

--- include/c_procs/portability.h
+++ include/c_procs/portability.h
@@ -6,9 +6,18 @@ extern "C"
 {
 #endif
 
+#include <stdio.h>
+
 /* force switch to posix-compliant strerror_r instead of GNU for portability */
 extern int __xpg_strerror_r(int errcode, char* buffer, size_t length);
 #define strerror_r __xpg_strerror_r
+
+/* thread-safe portable strerror */
+inline char *strerror2(int errcode, char* buffer, size_t length) {
+	if (strerror_r(errcode, buffer, length) != 0)
+		snprintf(buffer, length, "failed to get error description: %d", errcode);
+	return buffer;
+}
 
 #ifdef __cplusplus
 }
